6155133057ace0fc5ceb95bf7fe89f36998a2793
Bwahoji <bwahoji@foxmail.com>
2025-03-28 17:06:17 +0800

    Added Scaffold

--- app/src/main/java/com/bwah/reviewlist/MainActivity.kt
+++ app/src/main/java/com/bwah/reviewlist/MainActivity.kt
@@ -1,15 +1,52 @@
 package com.bwah.reviewlist
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.automirrored.outlined.List
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.bwah.reviewlist.ui.theme.ReviewListTheme
 
@@ -19,29 +56,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ReviewListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Home()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class NavigationItem(val label: String, val icon: ImageVector)
 
-@Preview(showBackground = true)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    ReviewListTheme {
-        Greeting("Android")
+@Preview
+fun Home() {
+    val topBarScrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
+        rememberTopAppBarState()
+    )
+
+    val homeNavigationBarItem = listOf(
+        NavigationItem(stringResource(R.string.today), Icons.Filled.DateRange),
+        NavigationItem(stringResource(R.string.list), Icons.Filled.Menu),
+        NavigationItem(stringResource(R.string.statistics), Icons.Filled.Info)
+    )
+    var currentNavigationIndex by remember {
+        mutableStateOf(0)
+    }
+    Scaffold (
+        topBar = {
+            LargeTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground
+                ),
+                title = {
+                    Text(
+                        stringResource(R.string.app_name),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                scrollBehavior = topBarScrollBehavior
+            )
+        },
+        bottomBar = {
+            BottomAppBar (
+                containerColor = NavigationBarDefaults.containerColor,
+            ){
+                homeNavigationBarItem.forEachIndexed { index, navigationItem->
+                    NavigationBarItem(
+                        selected = currentNavigationIndex == index,
+                        onClick = {
+                            currentNavigationIndex = index
+                        },
+                        icon = {
+                            Icon(
+                                navigationItem.icon,
+                                contentDescription = null
+                            )
+                        },
+                        label = {
+                            Text(text = navigationItem.label)
+                        },
+                        alwaysShowLabel = true,
+                        colors = NavigationBarItemDefaults.colors()
+                    )
+                }
+            }
+        }
+    ){
     }
 }
