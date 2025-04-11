cc4049261881de7a6a6852efb261b596282ff980
hougou <2929519317@qq.com>
2025-04-11 16:57:03 +0800

    first add room

---
+++ app/src/main/java/com/bwah/reviewlist/reviewlist_database/db/AppDatabase.kt
@@ -0,0 +1,8 @@
+package com.bwah.reviewlist.reviewlist_database.db
+
+
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.bwah.reviewlist.reviewlist_database.db.Dao.UserDao
+import com.bwah.reviewlist.reviewlist_database.model.User
