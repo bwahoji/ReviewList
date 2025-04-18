22cd4a2dc5036bc8c59fa27e25b7d65b878eb412
Bwahoji <bwahoji@foxmail.com>
2025-04-18 17:27:53 +0800

    Changed design to Xiaomi's Alive Design(through MiuiX Library) Added List Page Added Tab Menu Added Analysis Page

---
+++ app/src/main/java/com/bwah/reviewlist/FAB.kt
@@ -0,0 +1,67 @@
+package com.bwah.reviewlist
+
+import androidx.compose.runtime.Composable
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import top.yukonga.miuix.kmp.basic.BasicComponent
+import top.yukonga.miuix.kmp.basic.BasicComponentDefaults
+import top.yukonga.miuix.kmp.basic.InputField
+import top.yukonga.miuix.kmp.icon.MiuixIcons
+import top.yukonga.miuix.kmp.basic.*
+import top.yukonga.miuix.kmp.theme.Colors
+
+@Composable
+fun FabUI(
+    padding: PaddingValues,
+    colors: Colors,
+    finish: (() -> Unit)
+) {
+    Scaffold (
+        contentColor = colors.surfaceContainer,
+        topBar = {
+            SmallTopAppBar(
+                title = stringResource(R.string.add_new_content),
+                color = colors.surfaceContainer,
+                actions = {
+                    top.yukonga.miuix.kmp.basic.IconButton(
+                        modifier = Modifier.padding(end = 21.dp).size(40.dp),
+                        onClick = finish
+                    ) {
+                        top.yukonga.miuix.kmp.basic.Icon(
+                            imageVector = Icons.Rounded.Check,
+                            contentDescription = "OK"
+                        )
+                    }
+                }
+            )
+        }
+    ){ padding ->
+        LazyColumn (
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 6.dp)
+                .fillMaxSize(),
+            contentPadding = padding
+        ){
+            
+        }
+    }
+
+}
